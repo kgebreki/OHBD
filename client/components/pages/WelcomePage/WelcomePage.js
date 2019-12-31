20e1ba04c721b8d8cd54f6c19ca2130f3fe37b5a
ksgebrekirstos98 <ksgebrekirstos98@gmail.com>
2019-12-31 05:48:41 -0500

    Initial commit

--- client/components/pages/WelcomePage/WelcomePage.js
+++ client/components/pages/WelcomePage/WelcomePage.js
@@ -18,7 +18,7 @@ export default function WelcomePage() {
       <div className="section">
         <div className="container">
           <h1 className="title is-1">
-            Welcome Page!
+            Welcome to OHDB!
           </h1>
         </div>
       </div>
